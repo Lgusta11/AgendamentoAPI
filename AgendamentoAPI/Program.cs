b050fa677489bf97b5b718193619d8e5a41cf93a
Luiz Gustavo Nogueira de Lima <112027359+Lgusta11@users.noreply.github.com>
2024-04-24 12:35:37 -0300

    Atualizar o Program.cs

--- AgendamentoAPI/Program.cs
+++ AgendamentoAPI/Program.cs
@@ -14,57 +14,48 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurações do DbContext, Identity, etc.
 builder.Services.AddDbContext<AgendamentosContext>();
-
 builder.Services.AddScoped<PessoaComAcesso>();
-
 builder.Services.AddIdentity<PessoaComAcesso, Admin>()
     .AddEntityFrameworkStores<AgendamentosContext>()
     .AddDefaultTokenProviders();
 
-builder.Services.AddSingleton<IEmailSender<PessoaComAcesso>, DummyEmailSender>();
+// Configuração do CORS
+builder.Services.AddCors(options => options.AddPolicy("wasm", policy => policy
+    .WithOrigins(builder.Configuration["BackendUrl"] ?? "https://localhost:7054")
+    .AllowAnyMethod()
+    .SetIsOriginAllowed(_ => true)
+    .AllowAnyHeader()
+    .AllowCredentials()));
 
-builder.Services.AddScoped<DAL<Professores>>();
-builder.Services.AddScoped<DAL<Equipamentos>>();
-builder.Services.AddScoped<DAL<Aulas>>();
-builder.Services.AddScoped<DAL<Agendamento>>();
-builder.Services.AddScoped<DAL<Admin>>();
-
+// Configuração do Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-
-builder.Services.AddCors(
-    options => options.AddPolicy(
-        "wasm",
-        policy => policy.WithOrigins([builder.Configuration["BackendUrl"] ?? "https://localhost:7054"])
-            .AllowAnyMethod()
-            .SetIsOriginAllowed(pol => true)
-            .AllowAnyHeader()
-            .AllowCredentials()));
+// Configuração do JSON Serializer
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(
+    options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
-app.UseCors("wasm");
-
+// Middleware para servir arquivos estáticos
 app.UseStaticFiles();
 
+// Configuração do JWT Authentication
+var issuer = builder.Configuration["Jwt:Issuer"];
+var key = builder.Configuration["Jwt:Key"];
+if (issuer == null || key == null)
+{
+    throw new Exception("Jwt:Issuer and Jwt:Key must be defined in the configuration.");
+}
 
-builder.Services.AddAuthentication(options =>
+app.UseAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(options =>
 {
-    var issuer = builder.Configuration["Jwt:Issuer"];
-    var key = builder.Configuration["Jwt:Key"];
-
-    if (issuer == null || key == null)
-    {
-        throw new Exception("Jwt:Issuer and Jwt:Key must be defined in the configuration.");
-    }
-
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
@@ -77,13 +68,13 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+// Configuração do CORS
+app.UseCors("wasm");
 
-builder.Services.AddAuthorization();
-
-
-app.UseAuthentication(); 
+// Middleware de Autorização
 app.UseAuthorization();
 
+// Adição dos Endpoints
 app.AddEndPointsProfessores();
 app.AddEndPointsAulas();
 app.AddEndPointsEquipamentos();
@@ -92,14 +83,16 @@ app.AddEndPointsAdmin();
 app.AddEndPointsCadastro();
 app.AddEndPoinsLogin();
 
-
+// Endpoint de Logout
 app.MapPost("auth/logout", async ([FromServices] SignInManager<PessoaComAcesso> signInManager) =>
 {
     await signInManager.SignOutAsync();
     return Results.Ok();
 }).WithTags("Autenticação");
 
+// Configuração do Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Inicialização do aplicativo
 app.Run();
