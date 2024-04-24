9c89a96952e11f9ffbe6122246266c2b5e3a103a
Luiz Gustavo Nogueira de Lima <112027359+Lgusta11@users.noreply.github.com>
2024-04-24 07:08:24 -0300

    Atualizar o LoginExtensions.cs

--- AgendamentoAPI/EndPoints/LoginExtensions.cs
+++ AgendamentoAPI/EndPoints/LoginExtensions.cs
@@ -1,11 +1,10 @@
-﻿using Agendamentos.Shared.Dados.Modelos;
-using Microsoft.AspNetCore.Identity.Data;
+using Agendamentos.Shared.Dados.Modelos;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace AgendamentoAPI.EndPoints
 {
@@ -13,60 +12,38 @@ namespace AgendamentoAPI.EndPoints
     {
         public static void AddEndPoinsLogin(this WebApplication app)
         {
-            var groupBuilder = app.MapGroup("auth")
-                .WithTags("Autenticação");
-
-            groupBuilder.MapPost("Login", async ([FromServices] UserManager<PessoaComAcesso> userManager, [FromServices] RoleManager<Admin> roleManager, [FromBody] LoginRequest loginRequest, IConfiguration _config) =>
+            app.MapPost("auth/login", async ([FromServices] UserManager<PessoaComAcesso> userManager, [FromBody] LoginRequest loginRequest, IConfiguration _config) =>
             {
                 var user = await userManager.FindByEmailAsync(loginRequest.Email);
-                if (user == null)
+                if (user == null || !(await userManager.CheckPasswordAsync(user, loginRequest.Password)))
                 {
-                    return Results.Redirect("/login");
-                }
-
-                var result = await userManager.CheckPasswordAsync(user, loginRequest.Password);
-                if (!result)
-                {
-                    return Results.Redirect("/home");
-                }
-
-                // Verifique a função do usuário
-                var roles = await userManager.GetRolesAsync(user);
-                if (roles.Contains("Admin"))
-                {
-                    return Results.Redirect("/Admin/Home");  // Alterado para "/Admin/Home"
-                }
-                else if (roles.Contains("Professor"))
-                {
-                    return Results.Redirect("/Home");  // Alterado para "/Home"
+                    return Results.Unauthorized(new { Message = "Email ou senha incorretos." });
                 }
 
                 // Geração do Token JWT
-                var claims = new[]
+                var claims = new List<Claim>
                 {
-                 new Claim(JwtRegisteredClaimNames.Sub, user.Email!),
-                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                 };
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id)
+                };
+
+                var userRoles = await userManager.GetRolesAsync(user);
+                claims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
 
                 var jwtKey = _config["Jwt:Key"];
-                if (jwtKey == null)
-                {
-                    throw new ArgumentNullException(nameof(jwtKey), "JwtKey cannot be null.");
-                }
-
                 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
                 var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-                var token = new JwtSecurityToken(_config["Jwt:Issuer"],
-                    _config["Jwt:Issuer"],
-                    claims,
+                var token = new JwtSecurityToken(
+                    issuer: _config["Jwt:Issuer"],
+                    audience: _config["Jwt:Issuer"],
+                    claims: claims,
                     expires: DateTime.Now.AddMinutes(30),
                     signingCredentials: creds);
 
                 return Results.Ok(new { Token = new JwtSecurityTokenHandler().WriteToken(token) });
-            });
-
-
+            }).WithTags("Autenticação");
         }
     }
 }
